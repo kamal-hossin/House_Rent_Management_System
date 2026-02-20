e850dc45eacfcb0f69a188eda12d1e4ee1df8bfc
Md Kamal Hossain <kamal.hossin.cse@gmail.com>
2026-02-21 00:21:35 +0600

    Update ALLOWED_HOSTS in settings.py
    Restrict allowed hosts for production environment.

--- smart_house_rent/smart_house_rent/settings.py
+++ smart_house_rent/smart_house_rent/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q0%ww)+f&v3(_(!yna2)juji0yk7(pdfp)q38&ex4%2$+h-ba4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['house-rent-management-system-woad.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['house-rent-management-system-woad.vercel.app']
 
 # Application definition
 
